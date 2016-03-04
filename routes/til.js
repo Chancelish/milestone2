03c113e0b7ce57bf0838ab43b4197ec00876982b
Chance Gallegos <chance.gallegos@gmail.com>
2016-03-03 19:50:07 -0700

    oh baby, a triple

--- routes/til.js
+++ routes/til.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var entries = [
+  {slug:"Node JS", body: "Node.js is really powerful, but, it feels like it was created in a hury. I'm not sure what all the operations are.", created_at: "3/3/2016"},
+  
+];
+
 /* GET til listing. */
 router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Today I Learned' });
+  res.render('til/index', { title: 'Today I Learned', entries: entries});
 });
 
 module.exports = router;
