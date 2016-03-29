f19b1ce75f9809a35b38b7aaaaf71aba54aabe7a
Chance Gallegos <chance.gallegos@gmail.com>
2016-03-29 17:37:31 -0600

    Possibly done

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,11 @@ router.post('/login', function(req, res, next) {
 		}
 	);
 	
-	
+});
+
+router.get('logout', function(req, res) {
+	res.clearCookie('username');
+	res.redirect("/");
 });
 
 module.exports = router;
--- routes/til.js
+++ routes/til.js
@@ -5,13 +5,14 @@ var entries = [];
 
 /* GET til listing. */
 router.get('/', function(req, res, next) {
+	var name = req.cookies.username || 'Anonymous';
 	req.db.driver.execQuery(
 		"SELECT * FROM todayIlearned;", 
 		function(anErorr, data){
 			if(anErorr) {
 				console.log(anError)
 			}
-			res.render('til/index', { title: 'Today I Learned', entries: data});
+			res.render('til/index', { title: 'Today I Learned', entries: data, name: name});
 		}
 	);
 });
@@ -22,8 +23,8 @@ router.get('/new', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 	req.db.driver.execQuery(
-		"INSERT INTO todayIlearned (slug,body) VALUES ('?','?');",
-		[req.body.slug, req.body.body],
+		"INSERT INTO todayIlearned (slug,body,author) VALUES ('?','?','?');",
+		[req.body.slug, req.body.body, req.cookie.username || 'Anonymous'],
 		function(anErorr, data){
 			if(anErorr) {
 				console.log(anError)
@@ -66,7 +67,8 @@ router.post('/:id', function(req, res, next) {
 
 //DELETE
 router.get('/:id/delete', function(req, res, next) {
-  req.db.driver.execQuery(
+	var name = req.cookies.username || 'Anonymous';
+    req.db.driver.execQuery(
 		'DELETE * FROM todayIlearned WHERE id=' + parseInt(req.params.id) + ';',
 		function(anError, data) {
 			if(anError) {
@@ -82,7 +84,7 @@ router.get('/:id/delete', function(req, res, next) {
 			if(anErorr) {
 				console.log(anError)
 			}
-			res.render('til/index', { title: 'Today I Learned', entries: data});
+			res.render('til/index', { title: 'Today I Learned', entries: data, name: name});
 		}
 	);
   
@@ -90,13 +92,14 @@ router.get('/:id/delete', function(req, res, next) {
 
 //
 router.get('/:id', function(req, res, next) {
+	var name = req.cookies.username || 'Anonymous';
 	req.db.driver.execQuery(
 		'SELECT * FROM todayIlearned WHERE id=' + parseInt(req.params.id) + ';',
 		function(anError, data) {
 			if(anError) {
 				consle.log(anError);
 			}
-			res.render('til/til', {title: "a entry", entry: data[0]});
+			res.render('til/til', {title: "a entry", entry: data[0], name: name});
 		}
 	);
 	
